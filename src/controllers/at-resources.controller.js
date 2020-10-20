6f9ec92a696f35e8541a4fe521534c24ece5e973
Oscar Sanchez <oscar.sanchez@agilethought.com>
2020-10-20 18:06:14 -0500

    Revert "Added at-resources views"
    This reverts commit f9eed9309876a2e05ffba991f16c2651cc932d0f.

--- src/controllers/at-resources.controller.js
+++ src/controllers/at-resources.controller.js
@@ -16,29 +16,9 @@ resourcesCtrl.backlog = (req, res) => {
     res.render("at-resources/backlog");
 };
 
-// Sprint
-resourcesCtrl.sprint = (req, res) => {
-    res.render("at-resources/sprint");
-};
-
-// Members
-resourcesCtrl.members = (req, res) => {
-    res.render("at-resources/members");
-};
-
-// Teams
-resourcesCtrl.teams = (req, res) => {
-    res.render("at-resources/teams");
-};
-
-// Mentors
-resourcesCtrl.mentors = (req, res) => {
-    res.render("at-resources/mentors");
-};
-
-// Metrics
-resourcesCtrl.metrics = (req, res) => {
-    res.render("at-resources/metrics");
+// Active Sprint
+resourcesCtrl.activeSprint = (req, res) => {
+    res.render("at-resources/activeSprint");
 };
 
 resourcesCtrl.tests = (req, res) => {
--- src/routes/at-resources.routes.js
+++ src/routes/at-resources.routes.js
@@ -1,15 +1,6 @@
 const router = require("express").Router();
 
-const {
-    dashboard,
-    backlog,
-    sprint,
-    members,
-    teams,
-    mentors,
-    metrics,
-    tests
-} = require("../controllers/at-resources.controller.js");
+const { dashboard, backlog, activeSprint, tests } = require("../controllers/at-resources.controller.js");
 
 const { isAdmin, isCareProvider } = require("../helpers/auth");
 
@@ -21,21 +12,10 @@ router.get("/dashboard", dashboard);
 // Backlog
 router.get("/backlog", backlog);
 
-// Sprint
-router.get("/sprint", sprint);
-
-// Members
-router.get("/members", members);
-
-// Teams
-router.get("/teams", teams);
-
-// Mentors
-router.get("/mentors", mentors);
-
-// Metrics
-router.get("/metrics", metrics);
+// Active Sprint
+router.get("/activeSprint", activeSprint);
 
 router.get("/test", tests);
+
 
 module.exports = router;
